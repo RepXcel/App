1914145ad50669288c9b9aaa16f04ed539f1dabd
Amanda Nguyen <77892730+AmandaNguye@users.noreply.github.com>
2024-01-20 18:51:25 -0700

    Add shared container components

---
+++ components/shared.tsx
@@ -0,0 +1,18 @@
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
+import { colors } from "./colors";
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: ${colors.white};
+`;
+
+export const BottomButtonContainer = styled.View`
+  width: 100%;
+  padding: 40px;
+  justify-content: flex-end;
+`;
+
+export const ScreenWidth = Dimensions.get("screen").width;
+export const ScreenHeight = Dimensions.get("screen").height;
