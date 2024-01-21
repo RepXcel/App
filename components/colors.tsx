9e68d93ba635f7fbecb147ad3bb953c74e59ae25
Amanda Nguyen <77892730+AmandaNguye@users.noreply.github.com>
2024-01-20 18:50:14 -0700

    Create color constants

---
+++ components/colors.tsx
@@ -0,0 +1,11 @@
+export const colors = {
+  white: "#ffffff",
+  primary: "#FF7B95",
+  secondary: "#18152D",
+  // tertiary: "#85c6d8",
+  tertiary: "#BADAFF",
+  gray: "#d1d5dB",
+  lightgray: "#f3f4f6",
+  darkgray: "#4b5563",
+  accent: "#fbcd77",
+};
