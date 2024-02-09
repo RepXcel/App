bb9525c0e8c276d2ceeac23125a38f5a4924be50
Amanda Nguyen <77892730+AmandaNguye@users.noreply.github.com>
2024-02-09 12:45:48 -0700

    Update colors

--- components/colors.tsx
+++ components/colors.tsx
@@ -1,11 +1,13 @@
 export const colors = {
   white: "#ffffff",
+  black: "#141414",
   primary: "#FF7B95",
-  secondary: "#18152D",
+  // primary: "#5792AD",
+  secondary: "#1F152D",
   // tertiary: "#85c6d8",
-  tertiary: "#BADAFF",
-  gray: "#d1d5dB",
-  lightgray: "#f3f4f6",
-  darkgray: "#4b5563",
+  tertiary: "#97C5FA",
+  gray: "#C5C5C5",
+  lightgray: "#BAABAB",
+  darkgray: "#77797B",
   accent: "#fbcd77",
 };
