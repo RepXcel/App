5d8006d2078c84eebac25edfad4f6dc88e49a9cc
Dat Lam <lamdat2551@gmail.com>
2024-02-26 12:12:14 -0700

    Fix bug for local storage Amplify

--- App.tsx
+++ App.tsx
@@ -10,19 +10,24 @@ import '@azure/core-asynciterator-polyfill';
 import { DataStore } from 'aws-amplify/datastore';
 import { User } from "./src/models";
 
-const App = async () => {
-  const testDatastore = async () => {
+const App = () => {
+  const [num, setNum] = useState(0);
+
+  const testSavingDatastore = async () => {
     try {
       const post = await DataStore.save(
         new User({
-          username: 'test',
+          username: `user${num}`,
         })
       );
+      setNum(num + 1);
       console.log('Username saved successfully!', post);
     } catch (error) {
       console.log('Error saving username', error);
     }
+  };
 
+  const testRetrievingDatastore = async () => {
     try {
       const users = await DataStore.query(User);
       console.log('Users retrieved successfully!', JSON.stringify(users, null, 2));
@@ -31,14 +36,42 @@ const App = async () => {
     }
   };
 
+  const testDeletingDatastore = async () => {
+    try {
+      await DataStore.clear();
+      console.log('User cleared successfully!');
+    } catch (error) {
+      console.log('Error deleting User', error);
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.heartRateTitleWrapper}>
+      </View>
       <TouchableOpacity
-        onPress={testDatastore}
+        onPress={testSavingDatastore}
         style={styles.ctaButton}
       >
         <Text style={styles.ctaButtonText}>
-          {"Test Datastore"}
+          {"Test Saving"}
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={testRetrievingDatastore}
+        style={styles.ctaButton}
+      >
+        <Text style={styles.ctaButtonText}>
+          {"Test Retrieving"}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={testDeletingDatastore}
+        style={styles.ctaButton}
+      >
+        <Text style={styles.ctaButtonText}>
+          {"Test Deleting"}
         </Text>
       </TouchableOpacity>
     </SafeAreaView>
